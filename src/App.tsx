1813bf4e3c812e43ac1b35f0593f5b94f5d5bd49
MohamedATeama <mohammed.teama789@gmail.com>
2026-02-17 00:17:20 +0200

    Folder Structure

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-background text-primary flex h-screen flex-col items-center justify-center text-center text-2xl font-bold">
+      <h1>GMATE</h1>
+    </div>
+  );
+};
+
+export default App;
